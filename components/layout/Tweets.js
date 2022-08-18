6d1975a905a184ce8c74e66580c5abbb68cfca52
Remlad <isiakaabd@gmail.com>
2022-08-18 22:59:12 +0100

    added likes

--- components/layout/Tweets.js
+++ components/layout/Tweets.js
@@ -5,13 +5,48 @@ import {
   TrashIcon,
   ShareIcon,
   HeartIcon,
-} from "@heroicons/react/solid";
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  setDoc,
+  doc,
+  onSnapshot,
+  collection,
+  deleteDoc,
+} from "firebase/firestore";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
-
+import { signIn, useSession } from "next-auth/react";
+import { db } from "../../firebase";
+import { useState, useEffect } from "react";
 const Tweets = ({ tweet }) => {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState([]);
+  const [hasLike, setHasLike] = useState(false);
   const { timestamp, image, username, userImage, name, text } = tweet.data();
-  console.log(timestamp);
+  const likePost = async () => {
+    if (session) {
+      if (hasLike) {
+        await deleteDoc(doc(db, "posts", tweet.id, "likes", session?.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", tweet.id, "likes", session.user.uid), {
+          username: session.user.username,
+        });
+      }
+    } else {
+      signIn();
+    }
+  };
+  useEffect(() => {
+    onSnapshot(collection(db, "posts", tweet.id, "likes"), (snapshot) =>
+      setLikes(snapshot.docs)
+    );
+  }, [tweet.id]);
+
+  useEffect(() => {
+    setHasLike(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+  }, [likes, session?.user.uid]);
+
   return (
     <div className="flex space-x-2 p-3 cursor-pointer border-b border-gray-200">
       <div>
@@ -43,12 +78,31 @@ const Tweets = ({ tweet }) => {
           alt="user"
         />
 
-        <div className="flex justify-between text-gray-500 p-2">
-          <ChatIcon className="h-9 w-9 p-2  hoverEffect hover:text-sky-blue-500 hover:bg-sky-100" />
-          <ShareIcon className="h-9 w-9  p-2 hoverEffect hover:text-sky-500 hover:bg-sky-100" />
-          <TrashIcon className="h-9 w-9  p-2 hoverEffect hover:text-red-600 hover:bg-sky-100" />
-          <HeartIcon className="h-9 w-9  p-2 hoverEffect hover:text-red-600 hover:bg-sky-100" />
-          <ChartBarIcon className="h-9 w-9 p-2  hoverEffect hover:text-sky-blue-500 hover:bg-sky-100" />
+        <div className="flex items-center justify-between p-2 w-100">
+          <ChatIcon className="h-10 w-10 p-2  hoverEffect hover:text-sky-blue-500 hover:bg-sky-100" />
+          <ShareIcon className="h-10 w-10  p-2 hoverEffect hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="h-10 w-10  p-2 hoverEffect hover:text-red-600 hover:bg-sky-100" />
+          <div className="flex gap-1 items-center justify-between">
+            {hasLike ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="h-10 w-10  p-2 hoverEffect text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-10 w-10  p-2 hoverEffect hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            <span
+              className={` ${hasLike && " text-red-600"} text-sm select-none ${
+                likes?.length <= 0 && "invisible"
+              }`}
+            >
+              {likes.length}
+            </span>
+          </div>
+          <ChartBarIcon className="h-9 w-9 p-2  hoverEffect hover:text-sky-blue-500 hover:bg-red-100" />
         </div>
       </div>
     </div>
--- components/pages/Feed.js
+++ components/pages/Feed.js
@@ -2,9 +2,8 @@ import { SparklesIcon } from "@heroicons/react/outline";
 import { HeaderForm, Tweets } from "components/layout";
 import { db } from "../../firebase";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { useId, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 export default function Feed() {
-  const id = useId();
   const [tweets, setTweets] = useState([]);
 
   useEffect(
@@ -17,28 +16,6 @@ export default function Feed() {
       ),
     []
   );
-
-  // const tweets = [
-  //   {
-  //     id,
-  //     name: "John Doe",
-  //     username: "johndoe",
-  //     time: "1h ago",
-  //     image:
-  //       "https://d1fdloi71mui9q.cloudfront.net/25oCZvSZTV684Q2YtnH4_uOYjRPFk5d2tXO11",
-  //     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
-  //   },
-  //   {
-  //     id: 1,
-  //     name: "John Doe1",
-  //     username: "johndoe",
-  //     time: "1h ago",
-  //     image:
-  //       "https://d1fdloi71mui9q.cloudfront.net/25oCZvSZTV684Q2YtnH4_uOYjRPFk5d2tXO11",
-  //     tweet:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
-  //   },
-  // ];
 
   return (
     <div className="sm:ml-[75px] xl:ml-[300px] w-[800px]  xl:min-w-6xl border-r border-l flex-2">
